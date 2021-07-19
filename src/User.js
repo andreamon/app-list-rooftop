9eb3697202d6844edfb044acfebe5031d14b22b3
Andrea Montaño <andrea14_06_2@hotmail.com>
2021-07-19 17:56:08 -0300

    add users into table

--- src/User.js
+++ src/User.js
@@ -4,15 +4,21 @@ import { Table, Spinner } from "react-bootstrap";
 
 function Users() {
   const baseURL = "https://fakerapi.it/api/v1/users";
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState([]);
+  console.log(users);
   useEffect(() => {
     axios.get(baseURL).then((res) => {
       setUsers(res.data);
+      console.log(res.data);
     });
   }, []);
   return (
     <div className="container-center">
-      {users ? (
+      {users.length === 0 ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Cargando...</span>
+        </Spinner>
+      ) : (
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -23,21 +29,22 @@ function Users() {
             </tr>
           </thead>
           <tbody>
-              
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
+          {users.length > 0 ? (
+            users.map((user, index) => {
+              return (
+                <tr key={user.uuid}>
+                  <td>{index}</td>
+                  <td>{user.firstname}</td>
+                  <td>{user.lastname}</td>
+                  <td>{user.username}</td>
+                </tr>
+              );
+            })
+          ):(
+              <tr><td>No hay datos</td></tr>
+          )}
           </tbody>
         </Table>
-      ) : (
-        // <div className="container-spinner">
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Cargando...</span>
-          </Spinner>
-        // </div>
       )}
     </div>
   );
