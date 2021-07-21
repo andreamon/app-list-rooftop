747f40ac2fbd392e4eec27121b09ff54945d5870
Andrea Montaño <andrea14_06_2@hotmail.com>
2021-07-21 18:36:21 -0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import { Container, Navbar, Nav } from "react-bootstrap";
-// import User from "./User.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "../src/components/Books";
 import Users from "../src/components/Users";
 
 function App() {
   return (
-    <>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React + FakerAPI</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Books />
-    <Users />
-    </>
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React + FakerAPI</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/books">Books</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Switch>
+        <Route path="/books">
+          <Books />
+        </Route>
+        <Route path="/">
+          <Users />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Table } from "react-bootstrap";
+import { Table, Pagination } from "react-bootstrap";
 
 function Users() {
-  const baseURL = "https://fakerapi.it/api/v1/users";
+  const baseURL = "https://fakerapi.it/api/v1/users?_quantity=50";
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   console.log(users);
   useEffect(() => {
     axios
@@ -17,30 +18,46 @@ function Users() {
   }, []);
 
   return (
-    <Table responsive size="sm">
-      <thead className="text-center">
-        <tr>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Nombre de Usuario</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.length > 0 ? (
-          users.map((user) => (
-            <tr key={user.uuid}>
-              <td>{user.firstname}</td>
-              <td>{user.lastname}</td>
-              <td>{user.username}</td>
-            </tr>
-          ))
-        ) : (
+    <>
+      <Table bordered hover responsive size="sm" className="table-center">
+        <thead className="table-head">
           <tr>
-            <td>NO SE ENCONTRARON DATOS</td>
+            <th>NAME</th>
+            <th>USERNAME</th>
+            <th>EMAIL</th>
+            <th>WEBSITE</th>
           </tr>
-        )}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {users.length > 0 ? (
+            users.map((user) => (
+              <tr key={user.uuid}>
+                <td>
+                  {user.firstname} {user.lastname}
+                </td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  <a href={user.website} rel="noreferrer" target="_blank">
+                    {user.website}
+                  </a>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>NO SE ENCONTRARON DATOS</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      {users.length !== 0 ? (
+        <Pagination className="paginate-center">
+          <Pagination.Prev />
+          <Pagination.Next />
+        </Pagination>
+      ) : null}
+    </>
   );
 }
 export default Users;
