344621b380346b0695cd91b9bb33c3277bec52a8
Andrea Montaño <andrea14_06_2@hotmail.com>
2021-07-23 11:45:16 -0300

    start with pagination

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,25 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Table, Pagination, Row, Col } from "react-bootstrap";
+import { Table, Row, Col } from "react-bootstrap";
 
 function Users() {
 	const baseURL = "https://fakerapi.it/api/v1/users?_quantity=50";
 	const [users, setUsers] = useState([]);
-	// const [currentPage, setCurrentPage] = useState(1);
-	console.log(users);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [usersPerPage, setUsersPerPage] = useState(10);
+	// console.log(users);
 	useEffect(() => {
 		axios
 			.get(baseURL)
 			.then((res) => {
 				setUsers(res.data.data);
-				console.log(res.data.data);
+				// console.log(res.data.data);
 			})
 			.catch((e) => console.log(e));
 	}, []);
+
+	let indexOfLastUser = currentPage * usersPerPage;
+	// let indexOfFirstUser = ;
 
 	return (
 		<>
@@ -40,14 +44,14 @@ function Users() {
 				</Col>
 			</Row>
 			<Row className="justify-content-center">
-				<Col md={8}>
+				<Col md={9}>
 					<Table bordered hover responsive size="sm" className="table-center">
 						<thead className="table-head">
 							<tr>
-								<th>NAME</th>
-								<th>USERNAME</th>
-								<th>EMAIL</th>
-								<th>WEBSITE</th>
+								<th>Name</th>
+								<th>Username</th>
+								<th>Email</th>
+								<th>Website</th>
 								{/* <th>IMAGE</th> */}
 							</tr>
 						</thead>
@@ -55,9 +59,9 @@ function Users() {
 							{users.length > 0 ? (
 								users.map((user) => (
 									<tr key={user.uuid}>
-										<td>
-											{/* <img width="10%" src={user.image} alt="image user" />{" "} */}
-											{user.firstname} {user.lastname}
+										<td className="name-detail">
+											<img width="50%" src={user.image} alt="image user" />{" "}
+											<span>{user.firstname} {user.lastname}</span>
 										</td>
 										<td>{user.username}</td>
 										<td>{user.email}</td>
