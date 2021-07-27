9a9ad2635472e7b16cf4f826cf0d5b8ddda6c6cf
andreamon <andrea14_06_2@hotmail.com>
2021-07-27 18:34:19 -0300

    add images & services folders

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,19 +3,19 @@ function Menu() {
 		<section className="menu-options">
 			<div>
 				<a href="/" className="link-css">
-					<img src="./users.png" alt="users" />
+					<img src="./images/users.png" alt="users" />
 					<p>Users</p>
 				</a>
 			</div>
 			<div>
 				<a href="/books" className="link-css">
-					<img src="./books.png" alt="books" />
+					<img src="./images/books.png" alt="books" />
 					<p>Books</p>
 				</a>
 			</div>
 			<div>
 				<a href="/products" className="link-css">
-					<img src="./products.png" alt="products" />
+					<img src="./images/products.png" alt="products" />
 					<p>Products</p>
 				</a>
 			</div>
